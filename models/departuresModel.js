7a593ed3df651cc721151330aec860232333db92
Tech-A39 <Tech-A39@Tech-A39s-MacBook-Air.local>
2016-12-23 04:43:12 +0000

    Minor Change

--- models/departuresModel.js
+++ models/departuresModel.js
@@ -2,20 +2,22 @@
 class DeparturesModel {
 
     constructor(obj){
-        //define arrays for parameters
         this.estimatedDeptTime = [];
         this.platformNum = [];
         this.origin = [];
         this.destination = [];
         this.scheduledDeptTime = [];
 
-        for(let i = 0; i < 10; i++){
-            this.estimatedDeptTime[i] = obj.GetStationBoardResult.trainServices.service[i].etd;
-            this.platformNum[i] = obj.GetStationBoardResult.trainServices.service[i].platform;
-            this.origin[i] = obj.GetStationBoardResult.trainServices.service[i].origin.location[0].locationName;
-            this.destination[i] = obj.GetStationBoardResult.trainServices.service[i].destination.location[0].locationName;
-            this.scheduledDeptTime[i] = obj.GetStationBoardResult.trainServices.service[i].std;
+        let trainServices = obj.GetStationBoardResult.trainServices.service;
+        
+        for(let i in trainServices) {
+            this.origin[i] = trainServices[i].origin.location[0].locationName;
+            this.destination[i] = trainServices[i].destination.location[0].locationName;
+            this.platformNum[i] = trainServices[i].platform;
+            this.estimatedDeptTime[i] = trainServices[i].etd;
+            this.scheduledDeptTime[i] = trainServices[i].std;
         }
+
     }
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,11 @@
 //Defualt Route to the Index Home Page
+
+/* Require Express for the routing, and indexController to render the page */
 const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/indexController");
 
+/* pass the Get request to the controller and show the homepage */
 router.get("/", indexController.showIndex);
 
 module.exports = router;
